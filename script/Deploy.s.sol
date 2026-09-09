669491291cec7b529336a90870bb163a73f95bc0
Cuong Tran <tranquoccuong@outlook.com.vn>
2026-09-09 16:05:45 +0700

    feat: add deployment config

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import './Base.s.sol';
 
 contract DeployScript is BaseRouterScript {
-  string salt = '';
+  string salt = '260909_933';
 
   /**
    * @dev Deploys KSAggregationRouterV3 to specified chains
